e4fdb44ca3cbb747e32be411e12d80f7ced16859
nataliedejohn <nmdejohn4@gmail.com>
2025-12-07 10:59:28 -0500

    Updated about message

--- app/src/pages/30_About.py
+++ app/src/pages/30_About.py
@@ -8,10 +8,12 @@ st.write("# About this App")
 
 st.markdown(
     """
-    This is a demo app for Data and Software in International Government and Politics Dialogue 2025 Project Course.  
+    This is a demo app for students at Universities across the world to buy and sell used items. 
 
-    The goal of this demo is to provide information on the tech stack 
-    being used as well as demo some of the features of the various platforms. 
+    The goal of this demo app is to provide an easy-to-use platform for students to find affordable items while
+    promoting sustainability by encouraging the reuse of goods. Oftentimes students need items for a short period of time
+    and buying new itms can be costly and wasteful. This app aims to solve that problem by connecting students
+    who want to sell their used items with other students who are looking for affordable options.
 
     Stay tuned for more information and features to come!
     """
